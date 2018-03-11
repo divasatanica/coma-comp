0379929cb22458d133f092d3648165ba03f4426b
divasatanica <jayhebes@vip.qq.com>
2018-03-11 20:36:16 +0800

    add: 初始化组件库根容器

---
+++ src/home/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="component-root"></div>
+</template>
+
+<script>
+/**
+ * @author coma
+ * @description 组件库根容器
+ */
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/home/layout/container.vue
@@ -0,0 +1,17 @@
+<template>
+  
+</template>
+
+<script>
+/**
+ * @author coma
+ * @description 布局容器
+ */
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
