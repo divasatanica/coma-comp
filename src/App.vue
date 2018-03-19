a9802da31179e52dd7c5febe75b4cca8ba6457f0
divasatanica <jayhebes@vip.qq.com>
2018-03-19 20:18:31 +0800

    update: 更改展示结构

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
 <template>
   <div id="app">
-    <router-view/>
+    <div class="left-nav">
+      <ul>
+        <li class="nav-item"><a href="./layout">Layout 布局</a></li>
+        <li class="nav-item"><a href="./button">Button 按钮</a></li>
+      </ul>
+    </div>
+    <div class="right-block">
+      <router-view/>
+    </div>
   </div>
 </template>
 
@@ -13,12 +21,28 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
 #app {
+  display: flex;
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+
+.left-nav {
+  width: 15%;
+  min-width: 120px;
+  height: 100vh;
+  background: #eaeaea;
+  .nav-item {
+    margin: 10px auto;
+  }
+}
+
+.right-block {
+  width: 85%;
+  height: 100vh;
 }
 </style>
